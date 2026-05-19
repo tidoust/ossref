3c8cd85d266e8225726b45ae0cb2c6342ae7a908
Francois Daoust <fd@tidoust.net>
2026-05-19 17:07:21 +0200

    Force key order in generated index.json file

--- scripts/build.ts
+++ scripts/build.ts
@@ -8,6 +8,8 @@ import { loadProjects } from "./load-projects";
 import { convertMarkdown } from "./text";
 import { ProjectsData } from "./types";
 
+import schema from "../schemas/data.schema.json" with { type: "json" };
+
 function convert() {
   const projects: ProjectsData = loadProjects();
   for (const project of Object.values(projects)) {
@@ -18,7 +20,51 @@ function convert() {
     }
   }
 
-  fs.writeFileSync("index.json", JSON.stringify(projects), "utf8");
+  // To ease updates and reduce arbitrary diffs, we're going to impose the
+  // order of the keys in the index.json file. This order needs to be
+  // revisited when a new key is created and when a key is removed.
+  let keyOrder = [
+    "name",
+    "description",
+    "description_html",
+    "homepage",
+    "repository",
+    "logo",
+    "owner",
+    "licenses",
+    "status",
+    "purposes",
+    "categories",
+  ];
+  const allKeys = Object.keys(schema.definitions.ProjectData.properties).filter(
+    (k) => k !== "id",
+  );
+  const missingKeys = allKeys.filter((k) => !keyOrder.includes(k));
+  if (missingKeys.length) {
+    throw new Error(
+      `Add ${missingKeys.join(", ")} to key order in build script`,
+    );
+  }
+  const droppedKeys = keyOrder.filter((k) => !allKeys.includes(k));
+  if (droppedKeys.length) {
+    throw new Error(
+      `Drop ${droppedKeys.join(", ")} from key order in build script`,
+    );
+  }
+
+  // Note: there may be a way to serialize things with one JSON.stringify call
+  // instead of going through entries and semi-serializing things on our own,
+  // but it works.
+  const res =
+    "{" +
+    Object.entries(projects)
+      .map(
+        ([id, project]) =>
+          JSON.stringify(id) + ": " + JSON.stringify(project, keyOrder),
+      )
+      .join(",") +
+    "}";
+  fs.writeFileSync("index.json", res, "utf8");
 }
 
 convert();
